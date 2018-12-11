9f304858d0aeef8809cbf0c62a67c9a664040563
Wilhelm <will@homeflow.co.uk>
2018-12-11 15:08:50 +0000

    add postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_03_141357) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.integer "day"
